ac080ab59294932f29681ec20e648176ece41cf6
Noor hayat <144549805+inoorhayat@users.noreply.github.com>
2024-07-17 19:59:02 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://chat-app-1r0d.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
